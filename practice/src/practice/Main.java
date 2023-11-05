20ffdead14ce42c921dacf7a399bfa2cd71b79ec
m-fukushi <fukushi.megumi.02@gmail.com>
2023-11-05 21:33:22 +0900

    ロケール/タイムゾーン

--- practice/src/practice/Main.java
+++ practice/src/practice/Main.java
@@ -1,14 +1,33 @@
 package practice;
+import java.util.TimeZone;
+
 
 public class Main {
 
 	public static void main(String[] args) throws Exception{
-		boolean isErr = true;
-		if(isErr) {
-			System.out.println("データが壊れています。異常終了します");
-			System.exit(1);
+//		//デフォルトロケールの取得
+//		//Locale loc = Locale.getDefault();
+//		Locale loc = Locale.getDefault();
+//		System.out.println(loc.getCountry()+"-"+loc.getLanguage());
+//		String now = (new SimpleDateFormat()).format(new Date());
+//		if(loc.getLanguage().equals("ja")) {
+//			System.out.println("現在の時刻は"+now);
+//		}
+//		else {
+//			System.out.println("Current time is"+now);
+//		}
+//		
+		TimeZone tz = TimeZone.getDefault();
+		System.out.print("現在のタイムゾーン：");
+		System.out.println(tz.getDisplayName());
+		if(tz.useDaylightTime()) {
+			System.out.println("夏時間を採用しています");
 		}
-		System.out.println("正常終了しました");
+		else {
+			System.out.println("夏時間を採用していません");
+		}
+		System.out.println("世界標準時との時差は");
+		System.out.println(tz.getRawOffset()/3600000+"時間");
 	}
 }
 	
