fb60cfc524665957c425a45ebbca4b997911800a
m-fukushi <fukushi.megumi.02@gmail.com>
2023-11-28 23:54:15 +0900

    6.4まで

--- practice/src/practice/Main.java
+++ practice/src/practice/Main.java
@@ -1,23 +1,56 @@
 package practice;
-import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException{
+    	//通常try-catch
+//    	FileWriter fw = null;
+//    	try {
+//    		fw = new FileWriter("rpgsave.dat",true);
+//    		fw.write('A');
+//    		fw.flush();
+//    	}catch(IOException e){
+//    		System.out.println("ファイル書き込みエラーです");
+//    	}finally {
+//    		if(fw != null) {
+//    			try {
+//    				fw.close();
+//    			}catch(IOException e2) {}
+//    		}
+//    	}
+    	
+    	//try-with-resourse
+    	try (
+    			FileWriter fw = new FileWriter("rpgsave.dat",true);
+    		)
+    	{    		
+    		fw.write('A');
+    		fw.flush();
+    	}    	
+    	catch(IOException e){
+    		System.out.println("ファイル書き込みエラーです");
+    	}
+    	
+    	
+//    	FileOutputStream fos = new FileOutputStream("rpgsave.dat",true);
+//    	fos.write(65);
+//    	fos.flush();
+//    	fos.close();
 //       FileWriter fw = new FileWriter("G:\\rpgsave.dat",true);
 //       fw.write('A');
 //       fw.flush();
 //       fw.close();
-       FileReader fr = new FileReader("G:\\rpgsave.dat");
-       System.out.println("すべてのデータを読んで表示します");
-       int i = fr.read();
-       while(i != -1) {
-    	   char c = (char)i;
-    	   System.out.println(c);
-    	   i= fr.read();
-       }
-       System.out.println("データの末尾に到達しました");
-       fr.close();
+//       FileReader fr = new FileReader("G:\\rpgsave.dat");
+//       System.out.println("すべてのデータを読んで表示します");
+//       int i = fr.read();
+//       while(i != -1) {
+//    	   char c = (char)i;
+//    	   System.out.println(c);
+//    	   i= fr.read();
+//       }
+//       System.out.println("データの末尾に到達しました");
+//       fr.close();
     }
 }
 
