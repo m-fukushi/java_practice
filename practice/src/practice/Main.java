8f65fa3af5bbd7983b8b0c5027f11f6f5806ea68
m-fukushi <fukushi.megumi.02@gmail.com>
2024-01-17 07:56:42 +0900

    スレッド練習16-1

--- practice/src/practice/Main.java
+++ practice/src/practice/Main.java
@@ -1,17 +1,24 @@
 package practice;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-
-      System.out.println("1円以上のアイテム一覧表を表示します");
-      ArrayList<Item> items = ItemsDAO.findByMinimumPrice(1);
-      for (Item item : items) {
-        System.out.printf("%10s%4d%4d", item.getName(), item.getPrice(), item.getWeight());
-      }
-    
- }
-	   
+	  System.out.println("止めるには「STOP」を入力してください");
+	  System.out.println("カウントダウンを開始します");
+//	  for(int i =9; i>=0; i--) {
+//		  System.out.print(i+"..");
+//		  try {
+//			  TimeUnit.SECONDS.sleep(1);
+//		  }catch(InterruptedException e) {
+//			  ;
+//		  }
+//	  }
+	  Thread t = new PrintingThread();
+	  t.start();
+	  String input = new Scanner(System.in).nextLine();
+	  System.out.println("入力文字列:"+input);
+	  System.out.println("停止処理は未作成です");
+  }	   
 }
